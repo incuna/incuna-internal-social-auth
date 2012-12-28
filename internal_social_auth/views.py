39561a89ea497776d980d3eda97fc2f75493528f
George Hickman <george@ghickman.co.uk>
2012-12-28 20:04:54 +0000

    Clean up the AuthComplete API a little

--- internal_social_auth/views.py
+++ internal_social_auth/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.contrib import messages
 from django.http import HttpResponseRedirect, HttpResponse
+from django.utils.encoding import force_text
 from django.views.generic.base import View
 from social_auth.exceptions import AuthFailed
 from social_auth.views import complete
@@ -17,12 +18,21 @@ class AuthComplete(View):
             return complete(request, backend, *args, **kwargs)
         except AuthFailed as e:
             logger.error(e)
-            messages.error(request, "Your Google Apps domain isn't authorized for this app")
-            return HttpResponseRedirect('/')
+            messages.error(request, self.get_error_message())
+            return HttpResponseRedirect(self.get_faiure_url())
+
+        def get_error_message(self):
+            if self.error_message:
+                return self.error_message
+            return "Your Google Apps domain isn't authorized for this app"
+
+        def get_failure_url(self):
+            if self.failure_url:
+                return force_text(self.failure_url)
+            return '/'
 
 
 class LoginError(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse(status=401)
-
 
