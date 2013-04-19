500fa30b29385a63aa946e8e6a4325ce3b6300e3
Philip Gardner <philip@incuna.com>
2013-04-19 12:10:21 +0100

    Version bump

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,12 @@
 Changelog for incuna-internal-social-auth
 =========================================
 
+v0.1.1
+----
+
+* django-social-auth dependency added.
+* README updated with better instructions.
+
 v0.1
 ----
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='incuna-internal-social-auth',
-    version='0.1',
+    version='0.1.1',
     url='http://github.com/incuna/incuna-internal-social-auth',
     packages=find_packages(),
     include_package_data=True,
@@ -15,4 +15,3 @@ setup(
         'django-social-auth==0.7.22',
     ],
 )
-
